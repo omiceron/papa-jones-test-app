706c14b1dd7f0710d336c14061eb8eb45a0ad551
Constantine Sharow <omiceron@gmail.com>
2019-04-01 14:53:04 +0300

    Add DriverCard

---
+++ components/DriverCard.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native'
+import PropTypes from 'prop-types'
+import FastestBus from '../components/FastestBus'
+
+class DriverCard extends Component {
+  static propTypes = {
+    firstName: PropTypes.string.isRequired,
+    middleName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    buses: PropTypes.array.isRequired,
+    dateOfBirth: PropTypes.object.isRequired,
+    onPress: PropTypes.func
+  }
+
+  render() {
+    const {firstName, middleName, lastName, buses, dateOfBirth, onPress} = this.props
+    const date = dateOfBirth.toDateString()
+
+    return <TouchableOpacity onPress = {onPress}>
+      <View style = {styles.container}>
+
+        <View style = {styles.textView}>
+          <Text>
+            {firstName} {middleName} {lastName}
+          </Text>
+        </View>
+
+        <View style = {styles.textView}>
+          <Text>
+            {date}
+          </Text>
+        </View>
+
+        <View style = {styles.textView}>
+          <Text>
+            {buses}
+          </Text>
+        </View>
+
+        <FastestBus buses = {buses}/>
+
+      </View>
+    </TouchableOpacity>
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 20,
+    backgroundColor: 'rgba(127,127,127, 0.1)'
+  },
+  textView: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 10
+  }
+})
+
+export default DriverCard
